3d5b503fbd65c5d300e9d767ac668135ad3c091e
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2022-04-01 23:49:26 +0200

    Added Product and ProductRating Models required.

---
+++ KitLapBackend/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KitLapBackend.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string ImageUrl { get; set; }
+        public float Price { get; set; }
+        public ProductRating Rating { get; set; }
+        public bool HasDiscount { get; set; } = false;
+        public int DiscountValue { get; set; } = 0;
+        public float DiscountedPrice { get; set; }
+
+        public Product()
+        {
+            DiscountedPrice = ((100 - DiscountValue) / 100) * Price;
+        }
+    }
+}
---
+++ KitLapBackend/Models/ProductRating.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KitLapBackend.Models
+{
+    public class ProductRating
+    {
+        public float Rate { get; set; }
+        public int Count { get; set; }
+    }
+}
