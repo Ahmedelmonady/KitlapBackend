2086a4baf563535f1b20b5b976fe30d5e2f0c1eb
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2022-04-02 14:14:42 +0200

    Added DataContext Class.

---
+++ KitLapBackend/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using KitLapBackend.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KitLapBackend.Data
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(DbContextOptions options): base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.UseCollation("utf8mb4_bin");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
