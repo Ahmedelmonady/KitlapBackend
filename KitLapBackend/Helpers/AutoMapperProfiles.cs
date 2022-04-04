fbb76a781432de7fb40429c0489e71d84052d286
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2022-04-04 20:17:26 +0200

    Added Product and Rating mappings

--- KitLapBackend/Helpers/AutoMapperProfiles.cs
+++ KitLapBackend/Helpers/AutoMapperProfiles.cs
@@ -12,10 +12,14 @@ namespace KitLapBackend.Helpers
     {
         public AutoMapperProfiles()
         {
+            //Mapping Product to ProductDto showing RatingsStats
             CreateMap<Product, ProductDto>()
                 .ForMember(dest => dest.DiscountedPrice, opt => opt.MapFrom(src => src.HasDiscount ? (1 - (src.DiscountRate / 100)) * src.Price : 0))
                 .ForMember(dest => dest.RatingStats, opt => opt.MapFrom(src => 
-                new RatingsStatsDto{ RatingsAverage = src.Rating.Average(val => val.Value), RatingsCount = src.Rating.Count()}));
+                new RatingsStatsDto{ RatingsAverage = src.Ratings.Average(val => val.Value), RatingsCount = src.Ratings.Count }));
+            
+            //Mapping Ratings to RatingsDto showing Values
+            CreateMap<Rating, RatingsDto>();
         }
     }
 }
