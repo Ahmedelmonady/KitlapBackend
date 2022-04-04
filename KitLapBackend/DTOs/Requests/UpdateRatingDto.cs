c131a5ac9ee49804f97ff68e4a91a10b5bb34aac
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2022-04-04 20:56:54 +0200

    Added UpdateRatingDto class

---
+++ KitLapBackend/DTOs/Requests/UpdateRatingDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KitLapBackend.DTOs.Requests
+{
+    public class UpdateRatingDto
+    {
+        [Required]
+        public int RatingId { get; set; }
+        [Required, Range(0,5)]
+        public float RatingValue { get; set; }
+    }
+}
