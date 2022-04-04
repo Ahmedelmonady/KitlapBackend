11b357e355348ee32d87a441e07d480f6cc16ae9
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2022-04-04 12:39:58 +0200

    Added Controllers required for Products, Categories, and Ratings with required injected services.

---
+++ KitLapBackend/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using KitLapBackend.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KitLapBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : BaseController
+    {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
+        public ProductsController(DataContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+    }
+}
