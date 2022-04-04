629b3a946fd6628e88b75b97b6aa966dcc2dd3e4
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2022-04-04 12:35:18 +0200

    Added Product to ProductDto Mapping Profile

--- KitLapBackend/Helpers/AutoMapperProfiles.cs
+++ KitLapBackend/Helpers/AutoMapperProfiles.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using KitLapBackend.DTOs;
+using KitLapBackend.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +12,10 @@ namespace KitLapBackend.Helpers
     {
         public AutoMapperProfiles()
         {
-
+            CreateMap<Product, ProductDto>()
+                .ForMember(dest => dest.DiscountedPrice, opt => opt.MapFrom(src => src.HasDiscount ? (1 - (src.DiscountRate / 100)) * src.Price : 0))
+                .ForMember(dest => dest.RatingStats, opt => opt.MapFrom(src => 
+                new RatingsStatsDto{ RatingsAverage = src.Rating.Average(val => val.Value), RatingsCount = src.Rating.Count()}));
         }
     }
 }
