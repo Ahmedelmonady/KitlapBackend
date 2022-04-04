a3c4a42c5a36d1a2c66032be3bc58f845e71e1e0
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2022-04-04 12:34:47 +0200

    Added ProductDto and RatingsStatsDto

---
+++ KitLapBackend/DTOs/ProductDto.cs
@@ -0,0 +1,29 @@
+﻿using KitLapBackend.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KitLapBackend.DTOs
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public List<Category> Categories { get; set; }
+        [Required]
+        public string ImageUrl { get; set; }
+        [Required]
+        public float Price { get; set; }
+        public RatingsStatsDto RatingStats { get; set; }
+        [Required]
+        public bool HasDiscount { get; set; }
+        public int DiscountRate { get; set; }
+        public float DiscountedPrice { get; set; }
+
+    }
+}
---
+++ KitLapBackend/DTOs/RatingsStatsDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KitLapBackend.DTOs
+{
+    public class RatingsStatsDto
+    {
+        public float RatingsAverage { get; set; }
+        public int RatingsCount { get; set; }
+    }
+}
