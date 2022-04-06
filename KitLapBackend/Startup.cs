50baf68e37a4b941274c70396cb883c2667962d6
Ahmed El Monady <ahmed_elmonady@hotmail.com>
2022-04-06 18:38:16 +0200

    Exposed Swagger documentation publicly

--- KitLapBackend/Startup.cs
+++ KitLapBackend/Startup.cs
@@ -37,7 +37,7 @@ namespace KitLapBackend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || true)
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
